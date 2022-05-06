a58c2b66eb52f546f7e46aa5058a49d3acfa8d74
minnthitoo <mto.forlearning@gmail.com>
2022-05-06 20:56:18 +0630

    variable

--- php_study/001_variable.php
+++ php_study/001_variable.php
@@ -11,4 +11,11 @@
     global $x; //to use global keyword 
     echo $x;
   }
+  //global viriables are stored in ($GLOBALS) arrays
+  $num1 = 10;
+  $num2 = 20;
+  $num3 = 30;
+  echo $GLOBALS['num1'].'<br>';
+  echo $GLOBALS['num2'].'<br>';
+  echo $GLOBALS['num3'].'<br>';
 ?>
